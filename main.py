ae281e3a1a2d5bcc4863000d2a7c7367f2390577
AICDEB <jens.schmidt@take-smart.com>
2019-07-30 16:33:22 +0200

    fix relative paths

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def run(uri, port, src, syntax):
         print("missing option --src")
         exit(1)
 
-    if not os.path.exists("out"):
-        os.makedirs("out")
+    if not os.path.exists(os.path.join(os.getcwd(),"out")):
+        os.makedirs(os.path.join(os.getcwd(),"out"))
 
 
     code_ast = []
 
-    reader = utils.DirReader(os.path.join(os.getcwd(), src[1:]))
+    reader = utils.DirReader(os.path.join(os.getcwd(), src))
     raw_files = reader.readDir()
 
     for raw_file in raw_files:
@@ -43,12 +43,12 @@ def run(uri, port, src, syntax):
     stripped_files = reader.readDirStripped()
 
     # write index html
-    utils.FileWriter("out/index.html", utils.HtmlBuilder(stripped_files, "Code Review Builder",uri, port, code_ast).getPage())
+    utils.FileWriter(os.path.join(os.getcwd(),"out/index.html"), utils.HtmlBuilder(stripped_files, "Code Review Builder",uri, port, code_ast).getPage())
 
     # generate others generic
     for file in raw_files:
         c_page = utils.HtmlPageBuilder(stripped_files, file, file,uri, port, syntax, code_ast)
-        utils.FileWriter("out/"+file.split(src.split("/")[1])[-1]+".html", c_page.getPage())
+        utils.FileWriter(os.path.join(os.getcwd(),"out/"+file.split(src.split("/")[1])[-1]+".html"), c_page.getPage())
 
 
 if __name__ == '__main__':
